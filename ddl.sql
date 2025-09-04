f26230f3d801d6e857dd6b0ea4eed0c7d5982e7d
avbakanin <avbakanin@gmail.com>
2025-09-04 20:24:09 +0200

    tables' structure was changed

--- ddl.sql
+++ ddl.sql
@@ -1,18 +1,20 @@
 CREATE TABLE public.users (
-	id int8 NOT NULL,
-	username text NULL,
-	first_name text NULL,
-	last_name text NULL,
-	CONSTRAINT users_pkey PRIMARY KEY (id)
+    id BIGINT NOT NULL,
+    username TEXT,
+    first_name TEXT,
+    last_name TEXT,
+    gender_preference TEXT DEFAULT 'female',
+    subscription_status TEXT DEFAULT 'free',
+    CONSTRAINT users_pkey PRIMARY KEY (id)
 );
 
 CREATE TABLE public.messages (
-	id serial4 NOT NULL,
-	user_id int8 NOT NULL,
-	"role" text NOT NULL,
-	"text" text NOT NULL,
-	created_at timestamp DEFAULT CURRENT_TIMESTAMP NULL,
-	CONSTRAINT messages_pkey PRIMARY KEY (id),
-	CONSTRAINT messages_user_id_fkey FOREIGN KEY (user_id) REFERENCES public.users(id) ON DELETE CASCADE
+    id SERIAL NOT NULL,
+    user_id BIGINT NOT NULL,
+    role TEXT NOT NULL,
+    text TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT messages_pkey PRIMARY KEY (id),
+    CONSTRAINT messages_user_id_fkey FOREIGN KEY (user_id) REFERENCES public.users(id) ON DELETE CASCADE
 );
 CREATE INDEX idx_messages_user_id_created_at ON public.messages USING btree (user_id, created_at DESC);
